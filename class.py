943a3b23067a39ea8aa392e806ba4e7589a216aa
IKEOFFIAH PIUS <piuspio12@gmail.com>
2022-04-04 13:14:12 +0000

    first commit

---
+++ class.py
@@ -0,0 +1,25 @@
+class KitchenStaff:
+    staff_id=''
+    first_name=''
+    last_name=''
+    role=''
+
+
+class Staff:
+    staff_id=''
+    first_name=''
+    last_name=''
+    department=''
+    role=''
+
+
+class Menu:
+    name=''
+    type_of_food=''
+
+
+class Coomodity_for_sale:
+    drinks=''
+    packs=''
+    extra_foods=''
+
